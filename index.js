afddec2cdefb2aa65d56929b4238120ec50d25ba
Utkp5 <utkp200@gmail.com>
2022-09-29 20:23:07 +0530

    backend

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+const logger = require("morgan");
+
 
 
 const dotenv = require('dotenv');
 dotenv.config();
 
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: false }));
+
+
+
+
+
+
+
+
+
+
 
 app.get("/", function(req,res) {
     return res.send("WORKING FINE");
 });
-
 
 
 app.listen(PORT, function (err) {
